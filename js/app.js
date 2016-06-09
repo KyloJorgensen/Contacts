2a6d37c56aa81d520b32610695dd29af225e471a
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-06-08 19:52:51 -0700

    add button on adds informaiton from input fields to an object. contacts are listed below new contact field. can click on a contact to view it in the current contact field.

--- js/app.js
+++ js/app.js
@@ -3,20 +3,58 @@ $(document).ready(function() {
 	var contacts = [];
 
 	$('body').on('click', '.add', function() {
-		varifyInputs();
-		console.log(contacts[0]);
-		console.log(contacts[0].firstName);
-		console.log(contacts[0].lastName);
-		console.log(contacts[0].phone);
-		console.log(contacts[0].street);
-		console.log(contacts[0].city);
-		console.log(contacts[0].state);
-
+		valaidateInputs();
+	})
+	.on('click', 'li', function() {
+		generateCurrentContact($(this).html());
 	});
 
-	function varifyInputs() { 
-		contacts.push({firstName: $('#firstName').val(), lastName: $('#lastName').val(), phone: $('#phone').val(), street: $('#street').val(), city: $('#city').val(), state: $('#state').val()});	
+	function valaidateInputs() {
+		var contactInfo = collectInputs();
+		for (var i = 0; i < contactInfo.length; i++) {
+			if (contactInfo[i] == '') {
+				alert('Please fill in all fields.');
+				return false;
+			}
+		}
+		createNewContact(contactInfo);
+		$('.contactInfo').val('');
 	}
 
+	// collects Inputs
+	function collectInputs() {
+		var contactInfo = [$('#firstName').val(), $('#lastName').val(), $('#phone').val(), $('#street').val(), $('#city').val(), $('#state').val()];
+		return contactInfo;
+	}
+
+	// create new contact
+	function createNewContact(contactInfo) {
+		var newContact = {firstName: contactInfo[0], lastName: contactInfo[1], phone: contactInfo[2], street: contactInfo[3], city: contactInfo[4], state: contactInfo[5]};
+		contacts.push(newContact);
+		updateContactList();
+	}
+
+	// updates contact list
+	function updateContactList() {
+		$('ul').empty();
+		for (var i = 0; i < contacts.length; i++) {
+			$('ul').append('<li>' + contacts[i].firstName + ' ' + contacts[i].lastName + '</li>')
+		}
+	}
+
+	// generates selected Contact in the Current Contact area
+	function generateCurrentContact(name) {
+		for (var i = 0; i < contacts.length; i++) {
+			if (name == (contacts[i].firstName + ' ' + contacts[i].lastName)) {
+				$('.fullName').text(contacts[i].firstName + ' ' + contacts[i].lastName)
+				$('.firstName').text(contacts[i].firstName)
+				$('.lastName').text(contacts[i].lastName)
+				$('.phone').text(contacts[i].phone)
+				$('.street').text(contacts[i].street)
+				$('.city').text(contacts[i].city)
+				$('.state').text(contacts[i].state)
+			}
+		}
+	}
 
 });
